2d700dd136feb38df869827f62b92244f3db891e
Justin Weiss <justin@uberweiss.org>
2010-11-09 12:27:44 -0800

    Fixed gem-as-plugin init.rb stuff

--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,4 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), 'lib/extra_assertions')
+require File.join(File.expand_path(File.dirname(__FILE__)), '../lib/extra_assertions')
 require 'active_support/test_case'
 
 # do this so we don't get deprecation warnings
